4f72717f150f8dab545fff0f578a8ab52027f4bf
TheMehedi <mehedibn96@gmail.com>
2019-02-20 16:49:52 +0600

    Complete design

--- app/src/main/java/com/themehedi/appinionassignment/MainActivity.java
+++ app/src/main/java/com/themehedi/appinionassignment/MainActivity.java
@@ -1,13 +1,27 @@
 package com.themehedi.appinionassignment;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Intern DCR");
+        toolbar.setTitleTextColor(Color.BLACK);
+
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+
     }
 }
